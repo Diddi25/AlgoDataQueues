9bbe4692b89b893bbebcce14fad0c597e15e6d30
Diddi <dps.dtt25@gmail.com>
2023-10-01 19:55:00 +0200

    think the ArrayQueue is done now

--- src/ArrayQueue.java
+++ src/ArrayQueue.java
@@ -1,5 +1,5 @@
 public class ArrayQueue<K extends Comparable<K>,V> extends Queue<K,V> {
-    private final Node<K,V>[] theQueue = new Node[10];
+    private Node<K,V>[] theQueue = new Node[10];
     private int first = 0;
     private int last = 0;
     private int queueLength = theQueue.length;
@@ -7,6 +7,9 @@ public class ArrayQueue<K extends Comparable<K>,V> extends Queue<K,V> {
     public void addAtFirst(Node<K,V> newItem) {
         if (last == queueLength - 1) {
             last = 0;
+        }
+        if(last == first - 1) {
+            allocateNewBiggerArray(first, last);
         }
         if (first == 0) {
             theQueue[first] = newItem;
@@ -32,5 +35,18 @@ public class ArrayQueue<K extends Comparable<K>,V> extends Queue<K,V> {
     @Override
     public boolean isEmpty() {
         return first == last;
+    }
+    private void allocateNewBiggerArray(int first, int last) {
+        Node<K,V>[] biggerQueue = new Node[queueLength*2];
+        for (int i = 0; i < queueLength - last; i++) {
+            biggerQueue[i] = this.theQueue[first];
+            first++;
+        }
+        for (int i = queueLength - last; i < queueLength - first; i++) {
+            biggerQueue[i] = this.theQueue[last];
+            first++;
+        }
+        this.theQueue = biggerQueue;
+        this.queueLength = queueLength*2;
     }
 }
